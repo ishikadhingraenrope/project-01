d5dad287c612806518f9b533eef86a72b491d95b
ishikadhingraenrope <ishika.dhingra@enropesolutions.com>
2025-07-27 21:19:57 +0530

    Text changes

--- my-vite-app/src/components/about/About.jsx
+++ my-vite-app/src/components/about/About.jsx
@@ -3,7 +3,7 @@ function About(){
     return(
         <>
         <div className="py-4 text-center">
-        <h1 className="text-2xl">Wecome to About page</h1>
+        <h1 className="text-2xl">Wecome to About page!</h1>
         </div>
         
         </>
